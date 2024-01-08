cdb2b1d7d5487e611aea99691fd8a8e178cb4be2
shalom-ins <insevm@insevm@trade>
2024-01-08 22:47:54 +0800

    feat(tokenuri): add svg for tokenURI returns
    - When ERC-7583 is recognized, ERC-7583 will no longer support tokenURI.
    - At this time, providing
    TokenURI is only a temporary measure to meet the needs of NFT Markets, and it will be discontinued
    in the future.

--- contracts/INS20.sol
+++ contracts/INS20.sol
@@ -5,6 +5,7 @@ import "contracts/ERC7583/IERC7583.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 struct Tick {
   string op;
@@ -38,8 +39,8 @@ contract INS20 is IERC7583, ERC721, Ownable{
   function inscribe(uint256 tokenId, bytes32[] calldata proofs) public {
     require(isInscribeOpen, "Is not open");
     require(tickNumber * mintLimit <= maxSupply, "Exceeded mint limit");
+    
     address owner = msg.sender;
-
     // merkle verify
     bytes32 leaf = keccak256(abi.encode(owner, tokenId));
     require(
@@ -73,8 +74,24 @@ contract INS20 is IERC7583, ERC721, Ownable{
   }
   
   /// @dev Won't support tokenURI anymore.
-  function tokenURI(uint256 tokenId) public pure override returns (string memory) {
-    return "";
+  function tokenURI(
+    uint256 tokenID
+  ) public view override returns (string memory) {
+    string memory output = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"> <style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="100" y="100" class="base">{</text><text x="130" y="130" class="base">"p":"ins-20",</text><text x="130" y="160" class="base">"op":"mint",</text><text x="130" y="190" class="base">"tick":"insc",</text><text x="130" y="220" class="base">"amt":1000</text><text x="100" y="250" class="base">}</text></svg>';
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"description": "INS20 is a social experiment, a first attempt to practice inscription within the EVM.", "image": "data:image/svg+xml;base64,',
+            Base64.encode(bytes(output)),
+            '"}'
+          )
+        )
+      )
+    );
+    output = string(abi.encodePacked("data:application/json;base64,", json));
+    return isFTOpen ? "Not support tokenURI any more." : output;
   }
 
   /**
