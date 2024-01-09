88db3e5bd78acf893936fb9678db0d010aa088ff
shalom-ins <insevm@insevm@trade>
2024-01-09 16:01:12 +0800

    feat(transfer ft): integrate FT's transfer into this
    Use a slot as the carrier for FT. Each user's first received NFT will be their Slot. The slot can
    only be transferred at the end, ensuring that after the transfer, the user's balance of both NFT and
    FT is zero. Once the FT function is activated, more slots can be minted. When a user conducts an FT
    transaction and the recipient does not have a slot, a new slot without any balance will be minted to
    carry the FT to be received. If the minting of slots reaches the limit, users can only purchase
    slots from the NFT market to accept FT. A user's FT balance will only be the balance of the Slot
    they hold. To use FT from other inscriptions, the waterToWine function must be used for conversion.

--- contracts/INS20.sol
+++ contracts/INS20.sol
@@ -6,24 +6,34 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
-
-struct Tick {
-  string op;
-  uint256 amt;
-}
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract INS20 is IERC7583, ERC721, Ownable{
-  uint128 public maxSupply; // 21,000,000
-  uint128 public mintLimit; // 1000
+  using Strings for uint256;
 
+  bytes public constant transferInsData = bytes('data:text/plain;charset=utf-8,{"p":"ins-20","op":"transfer","tick":"INSC+","amt":"1000"}');
+
+  uint64 public maxSupply; // 21,000,000
+  uint64 public mintLimit; // 1000
   // number of tickets minted
-  uint128 private tickNumber;
+  uint64 private tickNumber;
+  uint64 private tickNumberMax; // 21,000 * 3 = 63,000
+
   uint128 private _totalSupply;
   string private _tick;
 
   bytes32 public root;
   bool public isFTOpen;
   bool public isInscribeOpen;
+
+  // the FT slot of users. user address => slotId(insId)
+  mapping (address => uint256) public slotFT;
+  mapping (uint256 => bool) insTransferred;
+
+  // -------- IERC20 --------
+  // slot balance
+  mapping(uint256 => uint256) internal _balancesSlot;
+  mapping(address => mapping(address => uint256)) private _allowances;
 
   constructor(
     string memory tick,
@@ -34,9 +44,10 @@ contract INS20 is IERC7583, ERC721, Ownable{
 
   /// @notice This is the entry point for users who have qualified to inscribe new INSC tokens.
   /// @dev Before inscribing, you need to obtain the correct Merkle proofs.
+  /// @dev All inscriptions must be completed before FT can be opened.
   /// @param tokenId TokenId that will be inscribed.
   /// @param proofs Merkle proofs.
-  function inscribe(uint256 tokenId, bytes32[] calldata proofs) public {
+  function inscribe(uint256 tokenId, bytes32[] calldata proofs) public recordSlot(address(0), msg.sender, tokenId){
     require(isInscribeOpen, "Is not open");
     require(tickNumber * mintLimit <= maxSupply, "Exceeded mint limit");
     
@@ -50,16 +61,126 @@ contract INS20 is IERC7583, ERC721, Ownable{
 
     tickNumber++;
     _mint(owner, tokenId);
-    emit Inscribe(tokenId, bytes('"data:text/plain;charset=utf-8,{"p":"ins-20","op":"mint","tick":"INSC","amt":"1000"}'));
+    _balancesSlot[tokenId] = mintLimit;
+    emit Inscribe(tokenId, bytes('data:text/plain;charset=utf-8,{"p":"ins-20","op":"mint","tick":"INSC+","amt":"1000"}'));
   }
+
+  /**
+   *  --------- overide balance ---------
+   */
+
+  
+
+  /**
+   *  --------- overide approve ---------
+   */
+
+
 
   /**
    *  --------- overide transfer ---------
    */
+
+  /// @notice Transfer like FT for DeFi compatibility. Only the balance in the slot can be transferred using this function.
+  /// @dev Originally, it was not intended to use storage variables to store the data contained in inscriptions. However, for the sake of DeFi compatibility, storage variables are now used to store the balance of slot inscriptions.
+  /// @param to Receiver address
+  /// @return value The amount sent
+  function transfer(address to, uint256 value) public virtual returns (bool) {
+    address from = msg.sender;
+    require(isFTOpen, "The ability of FT has not been granted");
+    _transferFT(from, to, value);
+    return true;
+  }
+
+  function _transferFT(address from, address to, uint256 value) internal {
+    require(slotFT[from] != 0, "The sender must own a slot");
+
+    // Slots can be minted until the limit is reached.
+    if(slotFT[to] == 0){
+      require(tickNumber + 1 <= tickNumberMax, "The number of slots has reached the limit");
+      _mint(to, tickNumber);
+      slotFT[to] = tickNumber;
+      // _balancesSlot[slotFT[to]] = 0; // use default value
+      tickNumber++;
+    }
+
+    uint256 fromBalance = _balancesSlot[slotFT[from]];
+    require(fromBalance >= value, "Insufficient balance");
+
+    unchecked {
+      _balancesSlot[slotFT[from]] = fromBalance - value;
+    }
+    _balancesSlot[slotFT[to]] += value;
+
+    emit Transfer(from, to, value);
+    emit Inscribe(slotFT[from], bytes(string.concat(
+        '{"p":"ins-20","op":"transfer","tick":"INSC+","amt":"',
+        _balancesSlot[slotFT[from]].toString(),
+        '"}'
+      ))
+    );
+    emit Inscribe(slotFT[to], bytes(string.concat(
+        '{"p":"ins-20","op":"transfer","tick":"INSC+","amt":"',
+        _balancesSlot[slotFT[to]].toString(),
+        '"}'
+      ))
+    );
+  }
+
+  /// @notice You can freely transfer the balances between any two of your inscriptions, including slots.
+  /// @dev If compatibility with existing DeFi is not considered, the storage and computation of balance here would be unnecessary.
+  /// @param from Inscription with a decreased balance
+  /// @param to Inscription with a increased balance
+  /// @param amount The value you gonna transfer
+  function waterToWine(uint256 from, uint256 to, uint256 amount) public {
+    require(isFTOpen, "The ability of FT has not been granted");
+    require(ownerOf(from) == msg.sender && ownerOf(to) == msg.sender, "Is not yours");
+
+    uint256 fromBalance = _balancesSlot[from];
+    require(fromBalance >= amount, "Insufficient balance");
+    unchecked {
+      _balancesSlot[from] = fromBalance - amount;
+    }
+    _balancesSlot[to] += amount; 
+
+    emit Inscribe(from, bytes(string.concat(
+        '{"p":"ins-20","op":"transfer","tick":"INSC+","amt":"',
+        _balancesSlot[from].toString(),
+        '"}'
+      ))
+    );
+    emit Inscribe(to, bytes(string.concat(
+        '{"p":"ins-20","op":"transfer","tick":"INSC+","amt":"',
+        _balancesSlot[to].toString(),
+        '"}'
+      ))
+    );
+  }
+  
   /// @dev embed Inscribe event into Transfer of ERC721
-  function transferFrom(address from, address to, uint256 tokenId) public override {
-    ERC721.transferFrom(from, to, tokenId);
-    emit Inscribe(tokenId, bytes('"data:text/plain;charset=utf-8,{"p":"ins-20","op":"transfer","tick":"INSC","amt":"1000"}'));
+  function transferFrom(address from, address to, uint256 tokenIdOrAmount) public override returns(bool) {
+    if(!isFTOpen) {
+      // Moved the contents of 'recordSlot modify' here.
+      if (from == address(0)) _balancesSlot[tokenIdOrAmount] = mintLimit;
+      
+      if (from != address(0) && slotFT[from] == tokenIdOrAmount){
+        require(balanceOf(from) == 1, "Slot can only be transferred at the end");
+        slotFT[from] = 0;
+      } 
+
+      ERC721.transferFrom(from, to, tokenIdOrAmount);
+      if(!insTransferred[tokenIdOrAmount]) {
+        emit Inscribe(tokenIdOrAmount, transferInsData);
+        insTransferred[tokenIdOrAmount] = true;
+      }
+
+      if (to != address(0) && slotFT[to] == 0) {
+        slotFT[to] = tokenIdOrAmount;
+      }
+    }else{
+      // TODO: _spendAllowance(from, msg.sender, tokenIdOrAmount);
+      _transferFT(from, to, tokenIdOrAmount);
+    }
   }
 
   /// @dev embed Inscribe event into Transfer of ERC721
@@ -68,12 +189,15 @@ contract INS20 is IERC7583, ERC721, Ownable{
   }
 
   /// @dev embed Inscribe event into Transfer of ERC721
-  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override {
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override recordSlot(from, to, tokenId){
     ERC721.safeTransferFrom(from, to, tokenId, data);
-    emit Inscribe(tokenId, bytes('"data:text/plain;charset=utf-8,{"p":"ins-20","op":"transfer","tick":"INSC","amt":"1000"}'));
+    if(!insTransferred[tokenId]) {
+      emit Inscribe(tokenId, transferInsData);
+      insTransferred[tokenId] = true;
+    }
   }
   
-  /// @dev Won't support tokenURI anymore.
+  /// @dev Will close this function in the future.
   function tokenURI(
     uint256 tokenID
   ) public view override returns (string memory) {
@@ -108,5 +232,29 @@ contract INS20 is IERC7583, ERC721, Ownable{
 
   function openInscribe() public onlyOwner {
     isInscribeOpen = true;
+  }
+
+  /**
+   *  --------- modify ---------
+   */
+
+  /// @notice Slot can only be transferred at the end. If the user does not have a slot, then this tokenId will serve as his slot.
+  /// @dev This modify is used only for the transfer of NFTs.
+  /// @dev The balance of FT is only related to the slot.
+  /// @param from Sender
+  /// @param to Receiver
+  /// @param tokenId TokenID of NFT
+  modifier recordSlot(address from, address to, uint256 tokenId) {
+    // record the balance of the slot
+    if (from == address(0)) _balancesSlot[tokenId] = mintLimit;
+    
+    if (from != address(0) && slotFT[from] == tokenId){
+      require(balanceOf(from) == 1, "Slot can only be transferred at the end");
+      slotFT[from] = 0;
+    } 
+    _;
+    if (to != address(0) && slotFT[to] == 0) {
+      slotFT[to] = tokenId;
+    }
   }
 }
